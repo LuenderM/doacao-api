a25752db58f05866cc9eb1ab6cb5fa614443a825
Luender Meira <luender.meira@gmail.com>
2026-01-15 12:41:21 -0300

    Adicionando formatação de data para o campo dataCriacao em DoacaoDTO

--- src/main/java/com/doacao/api/dto/DoacaoDTO.java
+++ src/main/java/com/doacao/api/dto/DoacaoDTO.java
@@ -1,5 +1,6 @@
 package com.doacao.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,5 +10,7 @@ public class DoacaoDTO {
     private Long id;
     private Double valor;
     private String status;
+    
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime dataCriacao;
 }
